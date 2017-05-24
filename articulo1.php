92118a1baae8871e650d16581a7ed1976c7549e4
miguechava <salvadormiki@gmail.com>
2017-05-24 14:21:18 -0500

    Update articulo1.php

--- articulo1.php
+++ articulo1.php
@@ -1,54 +1,94 @@
 <!DOCTYPE html>
-<html lang="es">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
-<title>Primer artículo de ejemplo del nuevo sitio creado</title>
-<meta name="Description" content="Sencilla página que puede servir de ejemplo y base para  a partir de ella crear cualquier número de páginas similares para tu nuevo sitio.">
-<meta name="viewport" content="initial-scale=1.0, user-scalable=yes">
-<meta name="Robots" content="index, follow">
-<link href="style/estilo.css" rel="stylesheet" type="text/css" media="screen">
-</head> 
+<html>
+<style>
+form {
+    border: 3px solid #f1f1f1;
+}
+
+input[type=text], input[type=password] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+}
+
+button {
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+}
+
+button:hover {
+    opacity: 0.8;
+}
+
+.cancelbtn {
+    width: auto;
+    padding: 10px 18px;
+    background-color: #f44336;
+}
+
+.imgcontainer {
+    text-align: center;
+    margin: 24px 0 12px 0;
+}
+
+img.avatar {
+    width: 40%;
+    border-radius: 50%;
+}
+
+.container {
+    padding: 16px;
+}
+
+span.psw {
+    float: right;
+    padding-top: 16px;
+}
+
+/* Change styles for span and cancel button on extra small screens */
+@media screen and (max-width: 300px) {
+    span.psw {
+       display: block;
+       float: none;
+    }
+    .cancelbtn {
+       width: 100%;
+    }
+}
+</style>
 <body>
-<div id="inicio"></div>
-<div id="header">
-<?php    
-    include "include/logo1.php";   
-?>
-<div id="page">
-<div id="migaspan">
-<a href="index.php">Mi sitio</a> » 
-<a href="articulo1.php">Pagina de ejemplo</a> 
-</div>
-<h1>Primer artículo de ejemplo del nuevo sitio creado</h1>
-<?php    
-    include "include/compartir.php";   
-?>
 
-<div class="intro">
-Sencilla página que puede servir de ejemplo y base, para a partir de ella crear cualquier número de páginas similares para tu nuevo sitio web.
-</div>
+<h2>Login Form</h2>
 
-Esta es una sencilla página de ejemplo del sitio web que deseas crear.<br>
-Puedes crear varias como estas simplemente copiándolas y cambiando alguno de sus elementos.<br>
-Primeramente cambia el nombre del nuevo archivo creado.<br>
-A continuación ábrelo en un editor y cambia el título, la descripción y el contenido.<br>
-El texto puedes crearlo previamente en un editor como Microsoft Word, revisar la ortografía y gramática y copiarlo y pegarlo.<br>
-Separa los saltos de líneas con etiquetas &lt;br&gt;<br>
-Agrega imágenes, enlaces dentro del área del contenido.<br>
-<img style="margin:12px 0 12px 0;" src="img/hacer-paginas-php.jpg" width="620" height="400" alt="Imagen para el articulo" title="Primer artículo de ejemplo del nuevo sitio creado"><br>
-Por último crea un enlace en la página "index.php" al nuevo archivo es todo.<br>
-Finalmente agrega una nueva entrada en el archivo sitemap.xml para que los buscadores la indexen correctamente.<br>
-Después de subir los archivos modificados al servidor web, carga la nueva página en tu navegador y compártela en las redes sociales.<br>
-Cualquier duda lee más información detallada en: 
-<a href="http://norfipc.com/web/plantilla-gratis-php-para-crear-sitio-web-internet.php" target="_blank">Plantilla gratis en PHP para crear un sitio web de internet</a>
-<br><br>
+<form action="/action_page.php">
+  <div class="imgcontainer">
+    <img src="img_avatar2.png" alt="Avatar" class="avatar">
+  </div>
 
-<?php    
-    include "include/share.php";   
-?> 
-<?php    
-    include "include/footer.php";   
-?> 
-</div></div>
-</body> 
+  <div class="container">
+    <label><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
+
+    <label><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+        
+    <button type="submit">Login</button>
+    <input type="checkbox" checked="checked"> Remember me
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+</form>
+
+</body>
 </html>
